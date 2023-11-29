efb642c23c50408042a481c7295adee7fa4ce5e4
SeungWoo Lee <151738283+profiler00@users.noreply.github.com>
2023-11-29 21:06:06 +0900

    [Bronze V] Title: 사칙연산, Time: 132 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,10 @@
+const filepath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = require('fs').readFileSync(filepath).toString().trim().split('\n');
+
+const [a, b] = input[0].split(' ').map(x => parseInt(x));
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);

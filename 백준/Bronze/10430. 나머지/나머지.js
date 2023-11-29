6aed1bf64174144ea1d8f4c7a8cf648d3a5c70fb
SeungWoo Lee <151738283+profiler00@users.noreply.github.com>
2023-11-29 21:22:02 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,9 @@
+const filepath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = require('fs').readFileSync(filepath).toString().trim().split('\n');
+
+const [a, b, c] = input[0].split(" ").map((x) => parseInt(x));
+
+console.log((a + b) % c);
+console.log(((a % c) + (b % c)) % c);
+console.log((a * b) % c);
+console.log(((a % c) * (b % c)) % c);

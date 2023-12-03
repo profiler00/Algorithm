2c9bec4100128ac8a779630eacc5efdc917eb3b3
SeungWoo Lee <151738283+profiler00@users.noreply.github.com>
2023-12-04 01:09:18 +0900

    [Bronze V] Title: A+B - 7, Time: 120 ms, Memory: 9384 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,13 @@
+const filepath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = require('fs').readFileSync(filepath).toString().trim().split('\n');
+
+const T = parseInt(input[0]);
+
+let result = '';
+
+for (let i = 1; i <= T; i++) {
+    const [A, B] = input[i].split(' ').map((x) => parseInt(x));
+    result += `Case #${i}: ${A + B + '\n'}`
+}
+
+console.log(result);

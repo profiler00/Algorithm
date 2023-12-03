9d3fc174e6cd58c65f64315a9f0c6fe1749af7f2
SeungWoo Lee <151738283+profiler00@users.noreply.github.com>
2023-12-04 00:58:24 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 116 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
@@ -3,12 +3,12 @@ const input = require('fs').readFileSync(filepath).toString().trim().split('\n')
 
 const N = parseInt(input[0]);
 
-let result = '';
+let result = "";
 
 for (let i = 1; i <= N; i++) {
-    if (i % 4 === 0) {
-        result += 'long ';
-    }
+  if (i % 4 === 0) {
+    result += "long ";
+  }
 }
 
-console.log(result + 'int');
+console.log(result + "int");
